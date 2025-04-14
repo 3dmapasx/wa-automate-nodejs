28109774cae925349ec415a947250f0ae532ec21
3dmapasx <3dmapaseventos@gmail.com>
2025-04-14 01:36:02 -0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-
-# Atualiza os pacotes base
-apt update && apt install -y curl git bash
-
-# Instala o NVM (Node Version Manager)
+apt update && apt install -y curl git
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# Instala Node.js (versão 18 LTS)
+source "$NVM_DIR/nvm.sh"
 nvm install 18
 nvm use 18
-nvm alias default 18
-
-# Cria diretório do projeto
-mkdir -p /app
-cd /app
-
-# (Opcional) Clonar repositório do seu scraper
-# git clone https://github.com/seu-usuario/seu-repo.git .
-# cd seu-repo
-
-# Instala dependências do projeto
 npm install --legacy-peer-deps
